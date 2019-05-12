21b1cbf78e9f3faafa1bb59b8a0cb5c44dfbb425
Vikram Pratap Singh <vikram@geekyants.com>
2019-05-12 21:48:24 +0530

    initial push

---
+++ README.md
@@ -0,0 +1,32 @@
+# responsive_text_field
+
+A Flutter plugin to create ResponsiveTextField that auto resizes itself based on the input.
+It will increase its maxLines when the text overflows and decreases its maxLines when the text underflow.
+####Note: Provide a well calculated width to the widget after subtracting all the paddings and margins for it to work as expected.
+## Example
+You can just use it the same way you would use a TextField with just four required parameters:
+* minLines
+* maxLines
+* style
+* availableWidth
+
+Like the example below:
+```dart
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ResponsiveTextField(
+        availableWidth: MediaQuery.of(context).size.width,
+        minLines: 1,
+        maxLines: 5,
+        style: TextStyle(
+          fontSize: 16
+        ),
+      ),
+    );
+  }
+}
+```
+##Demo
+![responsive_text_field demo](responsive_text_field.gif)
---
+++ example/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_text_field/responsive_text_field.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Home()
+      )
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ResponsiveTextField(
+        availableWidth: MediaQuery.of(context).size.width-4,
+        minLines: 1,
+        maxLines: 5,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 5,horizontal: 0),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 2,color: Colors.red)
+          ),
+          border: OutlineInputBorder(
+              borderSide: BorderSide(width: 2,color: Colors.red)
+          )
+        ),
+        style: TextStyle(
+          fontSize: 16
+        ),
+      ),
+    );
+  }
+}
